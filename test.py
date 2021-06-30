48dc35c9ee9f170f802c70f7f5fecb0e6c5c1a44
cidricho <86400734+cidricho@users.noreply.github.com>
2021-06-30 15:42:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,17 @@ class CalculatorTest(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(self.calculator.subtract(3, 4), -1)
         self.assertEqual(self.calculator.evaluate("3 - 4"), -1)
-
+        
     def test_multiply(self):
         self.assertEqual(self.calculator.multiply(5, 6), 30)
         self.assertEqual(self.calculator.evaluate("5 * 6"), 30)
+        
+    def test_divide(self):
+        self.assertEqual(self.calculator.evaluate("8 / 4"), 2)
+
+    def test_evaluate_simple(self):
+        self.assertEqual(self.calculator.evaluate("1 + 1"), 2)
+
+    def test_evaluate_chain(self):
+        self.assertEqual(self.calculator.evaluate("2*-5-1/3.5+9"),
+                         -1.2857142857142865)
