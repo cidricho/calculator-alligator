b983f28868075a255b9469fb780c1a16a95e31e2
AngelikaFernandez <86413702+AngelikaFernandez@users.noreply.github.com>
2021-07-02 15:20:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class Calculator:
     """Main class for the logic of this application"""
-    def evaluate(self, exp: str) -> float:
+    def evaluate(self, exp: str) :
         """Evaluate expresssion from the string value in the display."""
         try:
             ans = eval(exp)
         except SyntaxError:
-            ans = "Error: Symtax Error"
+            ans = "Error: Syntax Error"
         except ZeroDivisionError:
             ans = "Error : Math Error"
         return ans 
