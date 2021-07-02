821df097a79b322a03a1ad0d9e598b9a42596e88
SheenaMBayan <86411852+SheenaMBayan@users.noreply.github.com>
2021-07-02 20:31:36 +0800

    Add files via upload

---
+++ ui.1.py
@@ -0,0 +1,11 @@
+b_sign = tk.Button (self, text='-')
+b_del = tk.Button (self, text='Del') 
+b_ac = tk.Button (self, text='AC')
+b_div = tk.Button (self, text='÷')
+
+b_sign.grid(row=2, column=0)
+b_del.grid(row=2, column=1)
+b_ac.grid(row=2, column=2)
+b_div.grid(row=2, column=3)
+
+
