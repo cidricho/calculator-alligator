d2ba23be4fec7dde2c2b8e65220c888f0e056d6f
Alhea Bess <86405532+alheabess012@users.noreply.github.com>
2021-07-07 16:44:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,53 +1,50 @@
-from main import App
-from main import Calculator
-
 import unittest
+from main import Calculator, App
 
-
-class CalculatorTest(unittest.TestCase):
-    """Test Cases for the Calculator Class"""
-    def setUp(self) -> None:
-        self.calculator = Calculator()
-
+class Test(unittest.TestCase):
+    
     def test_addition(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("1 + 2"), 3)
 
     def test_subtract(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("3 - 4"), -1)
 
     def test_multiply(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("5 * 6"), 30)
 
     def test_divide(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("8 / 4"), 2)
 
     def test_evaluate_simple(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("1 + 1"), 2)
 
     def test_evaluate_chain(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("2*-5-1/3.5+9"),
                          -1.2857142857142865)
 
     def test_syntax_error(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("1 +/ 1"),
                          "Error: Syntax Error")
 
     def test_math_error(self):
+        self.calculator = Calculator()
         self.assertEqual(self.calculator.evaluate("9/0"), "Error: Math Error")
 
-
-class GUITest(unittest.TestCase):
-    """
-    Test Cases for GUI class. 
-    """
-    def setUp(self) -> None:
+    def setUp(self):
         self.app = App()
-
-    def tearDown(self) -> None:
+    
+    def tearDown(self):
         self.app.destroy()
 
     def test_startup(self):
-        self.assertEqual(self.app.winfo_toplevel().title(), "Calculator")
+        self.assertEqual(self.app.winfo_toplevel().title(), "Calculator Alligator")
 
     def test_screen_set(self):
         self.app.display_string.set("string")
@@ -129,3 +126,6 @@ class GUITest(unittest.TestCase):
             self.app.display_string.set("2×5=10")
             self.app.action_operation(operation)
             self.assertEqual(self.app.display_string.get(), "10" + operation)
+            
+if __name__ == "__main__":
+    unittest.main()
